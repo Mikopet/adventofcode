e51a6b6199284a5521dc200b1ca3c54bde1f2430
Peter Mikola <mikopet@gmail.com>
2021-12-01 13:04:27 +0100

    #2021-dec1 reorganizing code for the second task

--- 2021/dec1/src/main.rs
+++ 2021/dec1/src/main.rs
@@ -1,9 +1,8 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+use std::process;
 
-fn main() -> io::Result<()> {
-    let file = File::open("input.txt")?;
-    let reader = BufReader::new(file);
+fn one<R: BufRead>(reader: &mut R) -> io::Result<u16> {
     let mut increment_count: u16 = 0;
     let mut previous_depth: u16 = 0;
 
@@ -15,6 +14,25 @@ fn main() -> io::Result<()> {
         previous_depth = depth;
     }
 
-    println!("{}", increment_count - 1);
-    Ok(())
+    Ok(increment_count - 1)
+}
+
+fn main() {
+    let file_result = File::open("input.txt");
+    let file;
+
+    match file_result {
+        Ok(f) => file = f,
+        Err(e) => {
+            println!("File open error! {}", e);
+            process::abort();
+        }
+    }
+
+    let mut reader = BufReader::new(file);
+
+    match one(&mut reader) {
+        Err(e) => println!("{:?}", e),
+        Ok(n) => println!("{:?}", n),
+    }
 }
