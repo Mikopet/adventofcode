005357602c39cac6e100dfbdae0f3408c08fedf2
Peter Mikola <mikopet@gmail.com>
2021-12-01 12:41:53 +0100

    #2021-dec1 first star

--- 2021/dec1/src/main.rs
+++ 2021/dec1/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+    let mut increment_count: u16 = 0;
+    let mut previous_depth: u16 = 0;
+
+    for line in reader.lines() {
+        let depth: u16 = line?.to_string().parse::<u16>().unwrap();
+        if depth > previous_depth {
+            increment_count += 1;
+        }
+        previous_depth = depth;
+    }
+
+    println!("{}", increment_count - 1);
+    Ok(())
 }
