310d0ec4a5397c62418a67b441fbc5a76dd56772
Peter Mikola <mikopet@gmail.com>
2021-12-04 21:24:49 +0100

    #2021-dec4 refact the whole first star

---
+++ 2021/dec4/src/one.rs
@@ -0,0 +1,10 @@
+use crate::shared::*;
+
+pub fn one(draws: Vec<u8>, matrices: Vec<Vec<u8>>) -> u32 {
+    let (matrix_index, draws_index) = matrix::least_bingo(&draws, &matrices);
+    let unmarked_sum =
+        matrix::calculate_unmarked(&matrices[matrix_index], &draws[..draws_index + 1]);
+    let winner_number = draws[draws_index] as u32;
+
+    unmarked_sum * winner_number
+}
---
+++ 2021/dec4/src/shared.rs
@@ -0,0 +1,112 @@
+pub mod matrix {
+    pub fn calculate_unmarked(matrix: &Vec<u8>, winner_draws: &[u8]) -> u32 {
+        let unmarked_numbers: Vec<u8> = matrix
+            .iter()
+            .filter(|n| !winner_draws.contains(n))
+            .cloned()
+            .collect();
+        unmarked_numbers
+            .iter()
+            .fold(0, |acc, x| acc + x.clone().clone() as u32)
+    }
+
+    pub fn least_bingo(draws: &Vec<u8>, matrices: &Vec<Vec<u8>>) -> (usize, usize) {
+        let mut least_position: usize = draws.len();
+        let mut matrix_index: usize = 0;
+
+        for (index, matrix) in matrices.iter().enumerate() {
+            let original_position = bingo_at(matrix, &draws);
+            let mut transposed = vec![0; 25];
+            transpose::transpose(&matrix, &mut transposed, 5, 5);
+            let transposed_position = bingo_at(&transposed, &draws);
+
+            if original_position < least_position || transposed_position < least_position {
+                matrix_index = index;
+                least_position = original_position.min(transposed_position);
+            }
+        }
+
+        (matrix_index, least_position)
+    }
+
+    fn bingo_at(matrix: &Vec<u8>, draws: &Vec<u8>) -> usize {
+        let mut current_draws: Vec<&u8> = vec![];
+        let mut bingo_position: usize = draws.len();
+
+        for (draw_index, draw) in draws.iter().enumerate() {
+            current_draws.push(draw);
+
+            let mut row: Vec<u8> = vec![];
+            let mut bingo: bool = false;
+            for n in matrix {
+                row.push(n.clone());
+                if row.len() == 5 {
+                    if row
+                        .iter()
+                        .filter(|&i| current_draws.contains(&i))
+                        .collect::<Vec<&u8>>()
+                        .len()
+                        == 5
+                    {
+                        bingo = true;
+                        break;
+                    }
+                    row = vec![];
+                }
+            }
+            if bingo {
+                bingo_position = draw_index;
+                break;
+            }
+        }
+
+        bingo_position
+    }
+}
+
+pub mod file {
+    use lazy_regex::*;
+    use std::io::{self, prelude::*};
+
+    pub static DIGITS_RE: Lazy<Regex> = lazy_regex!(r"\d+");
+
+    pub fn read<R: BufRead>(mut reader: R) -> io::Result<(Vec<u8>, Vec<Vec<u8>>)> {
+        let mut first_line = String::new();
+        reader
+            .read_line(&mut first_line)
+            .expect("Unable to read winner numbers");
+        let draw_numbers: Vec<u8> = find_digits(&first_line);
+
+        let mut matrices: Vec<Vec<u8>> = vec![];
+        let mut matrix: Vec<u8> = vec![];
+
+        for line in reader.lines() {
+            let row: String = line?.to_string();
+
+            if row.len() == 0 {
+                if matrix.len() > 0 {
+                    matrices.push(matrix)
+                }
+                matrix = vec![];
+                continue;
+            }
+
+            for n in find_digits(&row) {
+                matrix.push(n);
+            }
+        }
+
+        if matrix.len() > 0 {
+            matrices.push(matrix)
+        }
+
+        Ok((draw_numbers, matrices))
+    }
+
+    fn find_digits(s: &str) -> Vec<u8> {
+        DIGITS_RE
+            .find_iter(&s)
+            .filter_map(|d| d.as_str().parse::<u8>().ok())
+            .collect()
+    }
+}
