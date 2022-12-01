67ce2998cb7d7a40ecb2765539bf41024a05d938
Peter MIKOLA <peter.mikola@semantic-network.com>
2022-12-01 10:19:24 +0100

    #2022-dec1 second star + fix first

--- 2022/dec1/src/main.rs
+++ 2022/dec1/src/main.rs
@@ -15,8 +15,30 @@ fn one<R: BufRead>(reader: R) -> io::Result<usize> {
             }
         }
     }
+    elf_cals.push(counter);
 
     Ok(*elf_cals.iter().max().unwrap())
+}
+
+fn two<R: BufRead>(reader: R) -> io::Result<usize> {
+    let mut elf_cals: Vec<usize> = vec![];
+    let mut counter: usize = 0;
+
+    for line in reader.lines() {
+        match line?.to_string().parse::<usize>() {
+            Ok(i) => counter += i,
+            Err(_e) => {
+                elf_cals.push(counter);
+                counter = 0;
+            }
+        }
+    }
+
+    elf_cals.push(counter);
+    elf_cals.sort();
+
+    let lasts = &elf_cals[(elf_cals.len()-3)..];
+    Ok(lasts.iter().sum())
 }
 
 fn main() {
@@ -36,6 +58,13 @@ fn main() {
         Err(e) => println!("{:?}", e),
         Ok(n) => println!("{:?}", n),
     }
+
+    (&mut &file).rewind().ok();
+
+    match two(&mut reader) {
+        Err(e) => println!("{:?}", e),
+        Ok(n) => println!("{:?}", n),
+    }
 }
 
 #[cfg(test)]
@@ -44,7 +73,14 @@ mod tests {
 
     #[test]
     fn test_one() {
-        let cursor = io::Cursor::new(b"1000\n2000\r\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000");
+        let cursor = io::Cursor::new(b"1000\n2000\n\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000");
         assert_eq!(one(cursor).unwrap(), 24000);
     }
+
+    #[test]
+    fn test_two() {
+        let cursor = io::Cursor::new(b"1000\n2000\n\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000");
+        assert_eq!(two(cursor).unwrap(), 45000);
+    }
 }
+
